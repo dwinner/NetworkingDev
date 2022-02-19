4e596e9bda9a17931a783bd0f990823203bf2d00
Denis Vinevcev <dwinner@inbox.ru>
2022-02-19 15:56:12 +0300

    Blazor - raw examples

---
+++ Blazor/Blazor.ComponentsSample/Models/Book.cs
@@ -0,0 +1,9 @@
+﻿namespace Blazor.ComponentsSample.Shared;
+
+public class Book
+{
+    public Guid Id { get; set; }
+    public string Title { get; set; } = string.Empty;
+    public string? Publisher { get; set; }
+    public DateTime ReleaseDate { get; set; }
+}
